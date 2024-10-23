ef74c9709b11417e5e2c837dfadcfa3a649b901d
komal <komalhack143@gmail.com>
2024-10-23 19:47:24 +0530

    js changes

--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,12 @@ gsap.to("#nav",{
     backgroundColor:"#000",
     height:"60px",
     duration:0.5,
-    
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        markers:"body",
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1
+    }
 })
